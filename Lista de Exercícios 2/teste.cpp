029df7529f13ac6161355a96451f98db43907c40
Patrick Alvim <patricknunes98@gmail.com>
2022-09-12 17:13:21 -0700

    Changes to be committed: 	modified:   "Lista de Exerc\303\255cios 2/teste.cpp" 	modified:   "Lista de Exerc\303\255cios 2/teste.exe"

--- Lista de Exercícios 2/teste.cpp
+++ Lista de Exercícios 2/teste.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string.h>
+#include <vector>
+#include <array>
+#include <cmath>
 
 
 void SelectionSort(int vetor[])
@@ -63,16 +67,64 @@ void SequencialSearch(int vetor[], int value)
     }
 }
 
+void BruteForceStringMatch(char text[], char pattern[])
+{
+    int n = strlen(text);
+    int m = strlen(pattern);
+    int j;
 
+    for (int i = 0; i < n - m; i++)
+    {
+        j = 0;
+        while ((j < m) and (pattern[j] == text[i + j]))
+        {
+            j +=1;     
+        }
+        if (j == m)
+        {
+            std::cout << "A string esta no texto!";
+        }                 
+    }
+}
 
+void BruteForceClosestPair(std::vector<std::vector<int>> pontos)
+{
+    int d = 9999999999, i, j;
+
+    for (i = 0; i < pontos.size() - 1; i++)
+    {
+        for (j = 0; j < i+1; j++)
+        {
+            float k1 = std::pow((pontos[i][j] - pontos[i+1][j]), 2);
+            float k2 = std::pow((pontos[i+1][j+1] - pontos[i+2][j+1]), 2);
+            float k3 = k1 + k2;
+            std::cout << k3;
+        }
+        
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     int vetor[10] = {1,5,6,9,8,2,3,10,4,7};
-    //SelectionSort(vetor);
-    BubbleSort(vetor);
 
-    SequencialSearch(vetor, 7);
+    char text[] = "dwafggwkgwqghellewqeasdwhellodwd";
+    char pattern[] = "hello";
+
+    std::vector<std::vector<int>> pontos{{1, 3}, {4, 1}}; 
+
+    BruteForceClosestPair(pontos);
+
+    //for (int i = 0; i < pontos.size(); i++) {
+    //    for (int j = 0; j < pontos[i].size(); j++)
+    //        std::cout << pontos[i][j] << " ";
+    //    std::cout << std::endl;
+    //}
+    
+    //SelectionSort(vetor);
+    //BubbleSort(vetor);
+    //SequencialSearch(vetor, 11);
+    //BruteForceStringMatch(text, pattern);
 
     
     return 0;
